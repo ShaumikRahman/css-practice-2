f86c29dfd1ab6fb9818abf8025449af49d3c45d3
ShaumikRahman <37661645+ShaumikRahman@users.noreply.github.com>
2021-05-27 01:12:24 +0100

    final site

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
 const sidebar = document.querySelector('.sidebar');
 const sidebarButton = document.querySelector('.sidebar-button');
 const closeButton = document.querySelector('.close-button');
+const overlay = document.querySelector('.overlay');
+const body = document.querySelector('#body');
 
 sidebarButton.addEventListener('click', e => {
     if (sidebar.classList.contains('open')) {
         sidebar.classList.remove('open');
+        overlay.classList.remove('open');
+        body.classList.remove('scroll-control');
     } else {
         sidebar.classList.add('open');
+        overlay.classList.add('open');
+        body.classList.add('scroll-control');
     }
 })
 
 closeButton.addEventListener('click', e => {
     if (sidebar.classList.contains('open')) {
         sidebar.classList.remove('open');
+        overlay.classList.remove('open');
+        body.classList.remove('scroll-control');
     } else {
         sidebar.classList.add('open');
+        overlay.classList.add('open');
+        body.classList.add('scroll-control');
     }
 })
